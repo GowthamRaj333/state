80419a610321942c3b619e3274700b2ed33c4b7a
GowthamRaj333 <ranjithgowthamraj333@gmail.com>
2023-01-05 14:16:16 +0530

    sub

--- src/test/java/com/test/Sample.java
+++ src/test/java/com/test/Sample.java
@@ -13,5 +13,13 @@ System.out.println("KERALA");
 	private void delhi() {
 System.out.println("DELHI");
 }
+	
+	private void tvs() {
+		System.out.println("TVS");
+}
+	
+	private void hero() {
+		System.out.println("HERO");
+		}
 
 }
