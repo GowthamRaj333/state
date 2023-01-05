3118acbae1bbc85199850dfa1de846a77d0f9163
GowthamRaj333 <ranjithgowthamraj333@gmail.com>
2023-01-05 13:42:31 +0530

    default

---
+++ src/test/java/com/test/Sample.java
@@ -0,0 +1,17 @@
+package com.test;
+
+public class Sample {
+	
+	private void tamilnadu() {
+System.out.println("TAMILNADU");
+	}
+	
+	private void kerala() {
+System.out.println("KERALA");
+	}
+	
+	private void delhi() {
+System.out.println("DELHI");
+}
+
+}
